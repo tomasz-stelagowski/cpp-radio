32624eed2123df0c59650fa3a407ed1cabb12c54
tomasz-stelagowski <tomasz.stelagowski@gmail.com>
2018-09-05 23:40:59 +0200

    f

---
+++ time-driven-thread.cpp
@@ -0,0 +1,25 @@
+#include <thread>
+#include <string.h>
+#include <chrono>
+
+#include "time-driven-thread.hpp"
+
+time_driven_thread::time_driven_thread (int rtime){
+    this->rtime = rtime;
+    this->worker_thread = new std::thread(&time_driven_thread::process, this);
+}
+
+time_driven_thread::~time_driven_thread(){
+    worker_thread->join();
+}
+
+void time_driven_thread::process(){
+    
+    std::this_thread::sleep_for(std::chrono::miliseconds(rtime));
+
+    on_time_routine();
+}
+
+void time_driven_thread::join(){
+    worker_thread->join();
+}
--- time-driven-thread.hpp
+++ time-driven-thread.hpp
@@ -23,30 +23,4 @@ private:
     std::thread* worker_thread;
 };
 
-
-//Implementation moved here from message-driven-thread.cpp file
-//according to this: https://bytefreaks.net/programming-2/c/c-undefined-reference-to-templated-class-function
-//brief: having clas templates`s implementation in cpp file cause linker errors,
-// while its used in main, when calling its methods.
-
-time_driven_thread<T>::time_driven_thread (int rtime){
-    this->rtime = rtime;
-    this->worker_thread = new std::thread(&time_driven_thread::process, this);
-}
-
-time_driven_thread<T>::~time_driven_thread(){
-    worker_thread->join();
-}
-
-void time_driven_thread<T>::process(){
-    
-    std::this_thread::sleep_for(std::chrono::miliseconds(rtime));
-
-    on_time_routine();
-}
-
-void time_driven_thread<T>::join(){
-    worker_thread->join();
-}
-
 #endif
